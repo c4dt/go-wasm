6fc8922b0feb92d160ee5129446d2d9d41df72d9
tharvik <tharvik@users.noreply.github.com>
2020-05-01 23:01:35 +0200

    add call-function

---
+++ call-function/main.js
@@ -0,0 +1,20 @@
+async function run () {
+  const go = new Go()
+  const fetching = fetch('main.wasm')
+  const result = await WebAssembly.instantiateStreaming(fetching, go.importObject)
+  const running = go.run(result.instance)
+
+  while (!('go_wasm' in globalThis)) {
+    await new Promise(resolve => setTimeout(resolve))
+  }
+
+  const ret = go_wasm.increment(0)
+  if (ret instanceof Error) {
+    throw ret
+  }
+  console.log(ret)
+
+  await running
+}
+
+run().catch(console.error)
